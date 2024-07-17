cfa7c6d155869b6329e2cd30dcb913ccea7dd382
a.kanno <shkb1125@gmail.com>
2024-07-17 22:34:58 +0900

    'refactor:名前つきルート定義と共通bladeファイルの継承、add:共通bladeファイルをlayouts以下に作成(base.blade)'

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
