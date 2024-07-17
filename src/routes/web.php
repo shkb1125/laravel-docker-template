548bfb50e118d1ff357d21a94c519d456ce9fb3e
a.kanno <shkb1125@gmail.com>
2024-07-17 22:12:42 +0900

    'add:index.bladeにtodo追加ボタン作成、feat:create.bladeにtodo新規作成機能を追加'

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/todo', 'TodoController@index');
+Route::get('/todo/create', 'TodoController@create');
