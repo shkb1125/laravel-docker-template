0c64a2cc8e7518aeefb381d4902648475f214f8f
a.kanno <shkb1125@gmail.com>
2024-07-18 10:45:04 +0900

    'feat:todo新規追加後のDB保存まで機能を実装'

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/todo', 'TodoController@index');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
