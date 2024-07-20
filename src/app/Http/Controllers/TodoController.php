ebe6723ab4c050962fdb00b5c7359ea0f64c4aba
a.kanno <shkb1125@gmail.com>
2024-07-21 01:21:57 +0900

    refactor:TodoControllerにTodoクラスのインスタンスをコンストラクタに定義

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,15 +9,24 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
         // Todo(クラス)モデルをインスタンス化
-        $todo = new Todo();
+        // $todo = new Todo();
         // Todoオブジェクト(モデルオブジェクト)のall()を実行、返り値はcollectionクラス
         // DB::enableQueryLog();
-        $todos = $todo->all();
+        // $todos = $todo->all();
         // dd(DB::getQueryLog());
         // dd($todos);：collectionクラスのitemプロパティにarray(配列)でTodoインスタンスが格納されている
+
+        $todos = $this->todo->all();
 
         // 連想配列として$todosを渡している
         // 第二引数の連想配列は、[blade内での変数名 => 代入したい値]
@@ -37,18 +46,19 @@ class TodoController extends Controller
         今回は配列で'_token'と'content'の値を受け取っている
         */
         $inputs = $request->all();
-        // dd($inputs);
-
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
+        // $todo = new Todo();
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        // $todo->content = $inputs['content'];
+        // $todo->content = $inputs['content']; // 修正前
         // 2. Todoインスタンスのfill()を実行、引数に用意された配列の値をモデルのプロパティに代入
-        $todo->fill($inputs);
+        // $todo->fill($inputs);
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
         // DB::enableQueryLog();
-        $todo->save();
+        // $todo->save();
         // dd(DB::getQueryLog());
+
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         // redirect()関数に定義されているroute()関数を使用している、引数：名前定義したルート
         return redirect()->route('todo.index');
@@ -56,8 +66,10 @@ class TodoController extends Controller
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
+        // $model = new Todo();
+        // $todo = $model->find($id);
+
+        $todo = $this->todo->find($id);
 
         return view('todo.show', ['todo' => $todo]);
     }
